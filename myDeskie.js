03bf1f24598aa4eec2b8e605e5a43a0261cf8fed
Smilly1391 <smillygirl1391@gmail.com>
2025-06-15 11:33:20 -0400

    Update myDeskie.js

--- myDeskie.js
+++ myDeskie.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("todo");
     const input = document.getElementById("input");
-
-   
     const taskList = document.createElement("div");
     taskList.id = "task-list";
-    document.querySelector(".main").appendChild(taskList);
+    document.querySelector("#task").appendChild(taskList);
 
     form.addEventListener("submit", function (e) {
         e.preventDefault();
